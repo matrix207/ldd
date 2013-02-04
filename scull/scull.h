5f1d5d33438952d16a72e32c4b86a9e77f892576
Dennis <dennis.cpp@gmail.com>
2013-02-04 13:57:09 +0800

    add ldd3 scull source

--- scull/scull.h
+++ scull/scull.h
@@ -1,84 +1,177 @@
-// scull.h
+/*
+ * scull.h -- definitions for the char module
+ *
+ * Copyright (C) 2001 Alessandro Rubini and Jonathan Corbet
+ * Copyright (C) 2001 O'Reilly & Associates
+ *
+ * The source code in this file can be freely used, adapted,
+ * and redistributed in source or binary form, so long as an
+ * acknowledgment appears in derived source files.  The citation
+ * should list that the code comes from the book "Linux Device
+ * Drivers" by Alessandro Rubini and Jonathan Corbet, published
+ * by O'Reilly & Associates.   No warranty is attached;
+ * we cannot take responsibility for errors or fitness for use.
+ *
+ * $Id: scull.h,v 1.15 2004/11/04 17:51:18 rubini Exp $
+ */
 
 #ifndef _SCULL_H_
 #define _SCULL_H_
 
-#include <linux/fs.h>
-#include <linux/ioctl.h>
-#include <linux/cdev.h>
+#include <linux/ioctl.h> /* needed for the _IOW etc stuff used later */
 
-
-#define SCULLC_QUANTUM  4000
-#define SCULLC_QSET		500
-
-#define SCULLC_MAJOR 0
-#define SCULLC_DEVS 4
-
-/* file operator */
 /*
-  struct module *owner; 指向拥有该结构的模块的指针
-  loff_t (*llseek) (struct file *, loff_t, int); 修改文件当前读写位置
-  ssize_t (*read) (struct file *, char __user *, size_t, loff_t *); 从设备中读取数据
-  ssize_t (*aio_read) (struct kiocb *, char __user *, size_t, loff_t); 异步读取操作
-  ssize_t (*write) (struct file *, const char __user *, size_t, loff_t *); 向设备发送数据
-  ssize_t (*aio_write) (struct kiocb *, const __user *, size_t, loff_t *); 异步写入操作
-  int (*readdir) (struct file *, void *, filldir_t)
-  unsigned int (*poll) (struct file *, struct poll_table_struct *);
-  int (*ioctl) (struct inode *, struct file *, unsigned int, unsigned long);
-  int (*mmpa) (struct file *, struct vm_area_struct *); 映射设备内存到进程地址空间
-  int (*open) (struct inode *, struct file *);
-  int (*flush) (struct file *);
-  int (*release) (struct inode *, struct file *);
-  int (*fsync) (struct file *, struct dentry *, int);
-  int (*aio_fsync) (struct kiocb *, int);
-  int (*lock) (struct file *, int, struct file_lock *);
-  ssize_t (*readv) (struct file *, const struct iovec *, unsigned long, loff_t *);
-  ssize_t (*writev) (struct file *, const struct iovec *, unsigned long, loff_t *);
-  ssize_t (*sendfile) (struct file *, loff_t *, size_t, read_actor_t, void *);
-  ssize_t (*sendpage) (struct file*, struct page *, int, size_t, loff_t *, int);
-  unsigned long (*get_unmapped_area) (struct file *, unsigned long, unsigned long, unsigned long, unsigned long);
-  int (*check_flags) (int);
-  int (*dir_notify) (struct file*, unsigned long);
-*/
+ * Macros to help debugging
+ */
 
-struct file_operations scull_fops = {
-	.owner =	THIS_MODULE,
-	.llseek=	scull_llseek,
-	.read =		scull_read,
-	.write =	scull_write,
-	.ioctl =	scull_ioctl,
-	.open =		scull_open,
-	.release =	scull_release,
-};
-
-/* file structure */
-/*
-  mode_t f_mode; 文件模式
-  loff_t f_pos; 当前的读写位置
-  unsigned int f_flags; 文件标志
-  struct file_operations *f_op; 文件相关操作
-  void *private_data;
-  struct dentry *f_dentry; 文件对应的目录项结构
-*/
-
-/* inode structure */
-/*
-  dev_t i_rdev; 设备文件的inode结构
-  struct cdev *i_cdev; 字符设备结构体
-*/
-struct scull_dev {
-	struct scull_qset *data;
-	int quantum;
-	int qset;
-	unsigned long size;
-	unsigned int access_key;
-	struct semaphore sem;
-	struct cdev cdev;
-};
-
-struct scull_qset {
-    void **data;
-    struct scull_qset *next;
-};
-
+#undef PDEBUG             /* undef it, just in case */
+#ifdef SCULL_DEBUG
+#  ifdef __KERNEL__
+     /* This one if debugging is on, and kernel space */
+#    define PDEBUG(fmt, args...) printk( KERN_DEBUG "scull: " fmt, ## args)
+#  else
+     /* This one for user space */
+#    define PDEBUG(fmt, args...) fprintf(stderr, fmt, ## args)
+#  endif
+#else
+#  define PDEBUG(fmt, args...) /* not debugging: nothing */
 #endif
+
+#undef PDEBUGG
+#define PDEBUGG(fmt, args...) /* nothing: it's a placeholder */
+
+#ifndef SCULL_MAJOR
+#define SCULL_MAJOR 0   /* dynamic major by default */
+#endif
+
+#ifndef SCULL_NR_DEVS
+#define SCULL_NR_DEVS 4    /* scull0 through scull3 */
+#endif
+
+#ifndef SCULL_P_NR_DEVS
+#define SCULL_P_NR_DEVS 4  /* scullpipe0 through scullpipe3 */
+#endif
+
+/*
+ * The bare device is a variable-length region of memory.
+ * Use a linked list of indirect blocks.
+ *
+ * "scull_dev->data" points to an array of pointers, each
+ * pointer refers to a memory area of SCULL_QUANTUM bytes.
+ *
+ * The array (quantum-set) is SCULL_QSET long.
+ */
+#ifndef SCULL_QUANTUM
+#define SCULL_QUANTUM 4000
+#endif
+
+#ifndef SCULL_QSET
+#define SCULL_QSET    1000
+#endif
+
+/*
+ * The pipe device is a simple circular buffer. Here its default size
+ */
+#ifndef SCULL_P_BUFFER
+#define SCULL_P_BUFFER 4000
+#endif
+
+/*
+ * Representation of scull quantum sets.
+ */
+struct scull_qset {
+	void **data;
+	struct scull_qset *next;
+};
+
+struct scull_dev {
+	struct scull_qset *data;  /* Pointer to first quantum set */
+	int quantum;              /* the current quantum size */
+	int qset;                 /* the current array size */
+	unsigned long size;       /* amount of data stored here */
+	unsigned int access_key;  /* used by sculluid and scullpriv */
+	struct semaphore sem;     /* mutual exclusion semaphore     */
+	struct cdev cdev;	  /* Char device structure		*/
+};
+
+/*
+ * Split minors in two parts
+ */
+#define TYPE(minor)	(((minor) >> 4) & 0xf)	/* high nibble */
+#define NUM(minor)	((minor) & 0xf)		/* low  nibble */
+
+
+/*
+ * The different configurable parameters
+ */
+extern int scull_major;     /* main.c */
+extern int scull_nr_devs;
+extern int scull_quantum;
+extern int scull_qset;
+
+extern int scull_p_buffer;	/* pipe.c */
+
+
+/*
+ * Prototypes for shared functions
+ */
+
+int     scull_p_init(dev_t dev);
+void    scull_p_cleanup(void);
+int     scull_access_init(dev_t dev);
+void    scull_access_cleanup(void);
+
+int     scull_trim(struct scull_dev *dev);
+
+ssize_t scull_read(struct file *filp, char __user *buf, size_t count,
+                   loff_t *f_pos);
+ssize_t scull_write(struct file *filp, const char __user *buf, size_t count,
+                    loff_t *f_pos);
+loff_t  scull_llseek(struct file *filp, loff_t off, int whence);
+int     scull_ioctl(struct inode *inode, struct file *filp,
+                    unsigned int cmd, unsigned long arg);
+
+
+/*
+ * Ioctl definitions
+ */
+
+/* Use 'k' as magic number */
+#define SCULL_IOC_MAGIC  'k'
+/* Please use a different 8-bit number in your code */
+
+#define SCULL_IOCRESET    _IO(SCULL_IOC_MAGIC, 0)
+
+/*
+ * S means "Set" through a ptr,
+ * T means "Tell" directly with the argument value
+ * G means "Get": reply by setting through a pointer
+ * Q means "Query": response is on the return value
+ * X means "eXchange": switch G and S atomically
+ * H means "sHift": switch T and Q atomically
+ */
+#define SCULL_IOCSQUANTUM _IOW(SCULL_IOC_MAGIC,  1, int)
+#define SCULL_IOCSQSET    _IOW(SCULL_IOC_MAGIC,  2, int)
+#define SCULL_IOCTQUANTUM _IO(SCULL_IOC_MAGIC,   3)
+#define SCULL_IOCTQSET    _IO(SCULL_IOC_MAGIC,   4)
+#define SCULL_IOCGQUANTUM _IOR(SCULL_IOC_MAGIC,  5, int)
+#define SCULL_IOCGQSET    _IOR(SCULL_IOC_MAGIC,  6, int)
+#define SCULL_IOCQQUANTUM _IO(SCULL_IOC_MAGIC,   7)
+#define SCULL_IOCQQSET    _IO(SCULL_IOC_MAGIC,   8)
+#define SCULL_IOCXQUANTUM _IOWR(SCULL_IOC_MAGIC, 9, int)
+#define SCULL_IOCXQSET    _IOWR(SCULL_IOC_MAGIC,10, int)
+#define SCULL_IOCHQUANTUM _IO(SCULL_IOC_MAGIC,  11)
+#define SCULL_IOCHQSET    _IO(SCULL_IOC_MAGIC,  12)
+
+/*
+ * The other entities only have "Tell" and "Query", because they're
+ * not printed in the book, and there's no need to have all six.
+ * (The previous stuff was only there to show different ways to do it.
+ */
+#define SCULL_P_IOCTSIZE _IO(SCULL_IOC_MAGIC,   13)
+#define SCULL_P_IOCQSIZE _IO(SCULL_IOC_MAGIC,   14)
+/* ... more to come */
+
+#define SCULL_IOC_MAXNR 14
+
+#endif /* _SCULL_H_ */
